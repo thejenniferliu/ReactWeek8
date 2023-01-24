c03fb71d19f34664f2621d388cd532269d9b7ce1
Jennifer_Liu <thejenniferliu@gmail.com>
2023-01-24 09:22:03 -0700

    'betternow'

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 
 export default function Form(props) {
-
+ //first thing in array = current state
+ //second thing is a function that allows you to update the state
     const [todoItems, setTodoItems] = useState([])
     const [stuffToDo, setInputValue] = useState('');
-
+//first state = todoItems = an empty list 
+//second state = stufftodo = the actual input of stuff = a string
 
     const handleSubmit = e => {
         e.preventDefault();
-        if (stuffToDo=== '') return;
+        if (stuffToDo === '') return;
         setTodoItems([...todoItems, stuffToDo]);
         setInputValue('');
       };
@@ -20,18 +22,17 @@ export default function Form(props) {
             <label>
                 What's to do, {props.name}?
                 <input type = 'text' value = {stuffToDo} onChange = { e => setInputValue(e.target.value)} className = 'form-control text-center bg-light text-dark w-80'></input>
-                <input type = 'submit' value = 'DO IT' className = 'btn btn-outline-dark w-50 mt-2'></input>
+                <input type = 'submit' value = 'DO IT' className = 'btn btn-outline-dark w-50 mt-2 mb-3'></input>
             </label>
         </form>
-        <div>
-            
-            {todoItems.map((item, index) => (
-          <li key={index}>
-            {item}
-            </li>
-            ))}
-        
-        </div>
+            <div className = 'text-center'>
+
+                {todoItems.map((item, index) => (
+            <ul key={index}>
+                {index+1}. {item}
+                </ul>
+                ))}
+            </div>
 
         </div>
 
